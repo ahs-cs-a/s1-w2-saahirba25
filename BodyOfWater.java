83e400e766eefff9078cc72b39a37a1135e53f5a
saahirba25 <saahirba25@ausdk12.org>
2024-08-23 17:04:41 +0000

    BodyOfWater

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,35 @@
+public class BodyOfWater{
+    private String name;
+    private double largestDiameter;
+    private double AvgDepth;
+    private boolean isSaltwater;
 
+    public BodyOfWater(String n, double ld, double ad, boolean s){
+        name = n;
+        largestDiameter = ld;
+        AvgDepth = ad;
+        isSaltwater = s;
+    }
+    public BodyOfWater(String n, double ad){
+        name = n;
+        AvgDepth = ad;
+    }
+    public String getname(){
+        return name;
+    }
+    public double getlargestDiameter(){
+        return largestDiameter;
+    }
+    public double getAverageDepth(){
+        return AvgDepth;
+    }
+    public boolean getisSaltWater(){
+        return isSaltwater;
+    }
+    public void setname(String n){
+        name = n;
+    }
+    public void setAvgDepth(double ad){
+        AvgDepth = ad;
+    }
+}
--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,16 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
-        System.out.println("here");
+        BodyOfWater b = new BodyOfWater("Chad",400000,180,false);
+        BodyOfWater b1 = new BodyOfWater("Chad",180.0);
+        System.out.println(b.getname());
+        System.out.println(b.getlargestDiameter());
+        System.out.println(b.getAverageDepth());
+        System.out.println(b.getisSaltWater());
+        System.out.println(b1.getname());
+        System.out.println(b1.getAverageDepth());
+        b1.setname("Superior");
+        b1.setAvgDepth(406.0);
+        System.out.println(b1.getname());
+        System.out.println(b1.getAverageDepth());
     }
 }
